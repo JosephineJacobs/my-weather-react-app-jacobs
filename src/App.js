0c6d51d3efa321acc5286d285e86eddf040067b2
Josephine Jacobs <133575092+JosephineJacobs@users.noreply.github.com>
2024-11-02 10:23:59 +0100

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-     
+      <div className="container">
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/83802-josephine-lu-ching-jacobs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Josephine Jacobs
+          </a>{" "}
+          and is open-source on{" "}
+          <a
+            href="https://github.com/JosephineJacobs/my-weather-react-app-jacobs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          , published on{" "}
+          <a
+            href="hhttps://weatherstationreact.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>{" "}
+          .
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
